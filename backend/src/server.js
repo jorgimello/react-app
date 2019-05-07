bd3c69b1f40383f62518e21eaec31c2b289bae00
Jorge Ribeiro <joorgemelo@gmail.com>
2019-05-06 22:33:04 -0300

    Models added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://react-app-user:react-app-password@cluster0-s86zg.mongodb.net/react-db?retryWrites=true', {
+    useNewUrlParser: true,
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
