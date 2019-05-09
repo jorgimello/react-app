750f33296b4559e9599daf176a63ace0eb903cfc
Jorge Ribeiro <joorgemelo@gmail.com>
2019-05-08 23:24:14 -0300

    Creating controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.send('Hello Test');
-});
+const BoxController = require('./controllers/BoxController');
+
+routes.post('/boxes', BoxController.store);
 
 module.exports = routes;
