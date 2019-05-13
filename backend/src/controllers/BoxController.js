0324f7bf875874402411b40a0bc033f6a8f46850
Jorge Ribeiro <joorgemelo@gmail.com>
2019-05-12 21:48:04 -0300

    File upload module started

--- backend/src/controllers/BoxController.js
+++ backend/src/controllers/BoxController.js
@@ -1,8 +1,8 @@
 const Box = require('../models/Box');
 
 class BoxController {
-    async store(req, res) {
-        const box = await Box.create({ title: 'Rocketseat' });
+    async store(req, res) {        
+        const box = await Box.create(req.body);
         return res.json(box);
     }
 }
