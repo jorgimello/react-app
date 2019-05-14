35593c6bc3b185d1c480a6bcf11306b77f0c098e
Jorge Ribeiro <joorgemelo@gmail.com>
2019-05-13 23:39:26 -0300

    Creating new routes. GET box returned errors

--- backend/src/controllers/BoxController.js
+++ backend/src/controllers/BoxController.js
@@ -5,6 +5,11 @@ class BoxController {
         const box = await Box.create(req.body);
         return res.json(box);
     }
+
+    async show(req, res) {
+        const box = await Box.findById(req.params.id);
+        return res.json(box);
+    }
 }
 
 module.exports = new BoxController();
