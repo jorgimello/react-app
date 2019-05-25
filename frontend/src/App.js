5e8517d6777c2f8ba3e95d22b056fe76a4f9572e
Jorge Ribeiro <joorgemelo@gmail.com>
2019-05-25 19:31:19 -0300

    Still building frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.css';
 
-import Main from './pages/Main';
+import Routes from './routes'
 
-function App() {
-  return <Main />;
-}
+const App = () => <Routes />;
 
 export default App;
---
+++ frontend/src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Box from './pages/Box';
+
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route path='/' component={Main} />
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Routes;
