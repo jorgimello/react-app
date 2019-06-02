538ab9d0d44bf9d7fc1b96e1935de2aa12437574
Jorge Ribeiro <joorgemelo@gmail.com>
2019-06-02 14:40:24 -0300

    Fixing baseURL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://react-app-omnistack.herokuapp.com',
+  baseURL: 'https://react-app-omnistack-backend.herokuapp.com',
 });
 
 export default api;
