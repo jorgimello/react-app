496bca0b2e308d2987e177199040fdb26d36f6a3
Jorge Ribeiro <joorgemelo@gmail.com>
2019-06-12 20:21:35 -0300

    Changes to be committed: 	modified:   package.json 	modified:   src/pages/Box/index.js 	modified:   yarn.lock

--- mobile/src/pages/Box/index.js
+++ mobile/src/pages/Box/index.js
@@ -2,8 +2,56 @@ import React, { Component } from 'react';
 
 import { View } from 'react-native';
 
+import styles from './styles';
+import AsyncStorage from '@react-native-community/async-storage';
+import { distanceInWords } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import api from '../../services/api';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 export default class Box extends Component {
+  state = { box: {} };
+
+  async componentDidMount() {
+    // this.subscribeToNewFiles();
+
+    const box = await AsyncStorage.getItem('@RocketBox:box');
+    const response = await api.get(`boxes/${box}`);
+
+    this.setState({ box: response.data });
+  }
+
+  renderItem = ({ item }) => (
+    <TouchableOpacity
+      onPress={() => {}}
+      style={styles.file}
+    >
+      <View style={styles.fileInfo}>
+        <Icon name='insert-drive-file' size={24} color='#A5CFFF' />
+        <Text style={styles.fileTitle}>{item.title}</Text>
+      </View>
+
+      <Text style={styles.fileDate}>
+        há {distanceInWords(item.createdAt, new Date(), {locale: pt})}
+      </Text>
+    </TouchableOpacity>
+  );
+
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.boxTitle}>{this.state.box.title}</Text>
+
+        <Flatlist 
+          style={styles.list}
+          data={this.state.box.files}
+          keyExtractor={file => file._id}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          renderItem={this.renderItem}
+        />
+      </View>
+    );
   }
 }
